3db3e2040f70dfb2b74b3e39cface317c88eaa80
yyx <1959555480@qq.com>
2020-07-06 16:31:20 +0800

    提交

--- yyx-cli/src/views/Home.vue
+++ yyx-cli/src/views/Home.vue
@@ -4,36 +4,58 @@
       <!-- <img alt="Vue logo" src="@123/logo.png" /> -->
       <div id="nav">
         <router-link to="/about">About</router-link>
+        <div @click="clickShow">点击显示loading</div>
       </div>
-      <div class="home_color">
+      <!-- <div class="home_color">
         <div class="box">
           <div @click="to">22222</div>
         </div>
-      </div>
+      </div>-->
     </div>
+    <!-- <loading :visible="visible" text="加载中"></loading> -->
   </router-layout>
 </template>
 
 <script>
+// import loading from '@cp/Loading/Loading.vue'
+import Loading from "@cp/Loading";
 export default {
+  // components:{
+  //   loading
+  // },
   name: "Home",
+  data() {
+    return {
+      visible: true
+    };
+  },
   methods: {
     to() {
       this.$router.push({
         name: "About"
       });
+    },
+    clickShow() {
+      this.visible = !this.visible;
     }
   },
-  created() {
+  created() {},
+  mounted() {
+    const loading = Loading({ text: "正在加载。。。" });
+    // 三秒钟后关闭
+    setTimeout(() => {
+      loading.close();
+    }, 3000);
     console.log("没有缓存，加载home");
   }
 };
 </script>
 <style lang="less" scoped>
-.box {
-  height: 10000px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
+.home {
+  // height: 10000px;
+  // display: flex;
+  // flex-direction: column;
+  // justify-content: flex-end;
+  opacity: .1;
 }
 </style>
